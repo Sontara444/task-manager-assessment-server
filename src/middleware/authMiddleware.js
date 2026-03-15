98336eed1ce0bac466d5a6117c6b833c04854a69
Sontara444 <sontararajput@gmail.com>
2026-03-15 14:50:46 +0530

    some chnages

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,7 +5,9 @@ const User = require('../models/User');
 exports.protect = async (req, res, next) => {
   let token;
 
-  if (req.cookies.token) {
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.token) {
     token = req.cookies.token;
   }
 
